43f349a3fefdb5c60d2c95b1da545388661f3597
Josh Angelucci <jangelu@us.ibm.com>
2016-12-09 15:58:08 -0500

    more stuff

---
+++ pixiedust_flightpredict/pixiedust_flightpredict/templates/js/api.js
@@ -0,0 +1,55 @@
+// The Api module is designed to handle all interactions with the server
+
+{%import "commonExecuteCallback.js" as commons%}
+
+var Api = (function() {
+  var requestPayload;
+  var responsePayload;
+  var messageEndpoint = '/api/message';
+
+  // Publicly accessible methods defined
+  return {
+    sendRequest: sendRequest,
+
+    // The request/response getters/setters are defined here to prevent internal methods
+    // from calling the methods without any of the callbacks that are added elsewhere.
+    getRequestPayload: function() {
+      return requestPayload;
+    },
+    setRequestPayload: function(newPayloadStr) {
+      requestPayload = JSON.parse(newPayloadStr);
+    },
+    getResponsePayload: function() {
+      return responsePayload;
+    },
+    setResponsePayload: function(newPayloadStr) {
+      responsePayload = JSON.parse(newPayloadStr);
+    }
+  };
+
+  // Send a message request to the server
+  function sendRequest(text, context) {
+    // Build request payload
+    var payloadToWatson = {};
+    if (text) {
+      payloadToWatson.input = {
+        text: text
+      };
+    }
+    if (context) {
+      payloadToWatson.context = context;
+    }
+
+    var $$pixiedust_command = "from pixiedust_flightpredict.watsonMessageAPI import *" +
+      //"\nprint(getResponse('" + text + "'))";
+      "\nprint(getResponseTest())";
+
+    {% call(results) commons.ipython_execute("$$pixiedust_command", prefix) %}
+      {% if results["error"] %}
+        alert('error!');
+      {% else %}
+        Api.setResponsePayload({{results}});
+      {% endif %}
+    {% endcall %}
+  }
+}());
--- pixiedust_flightpredict/pixiedust_flightpredict/watsonMessageAPI.py
+++ pixiedust_flightpredict/pixiedust_flightpredict/watsonMessageAPI.py
@@ -1,16 +1,66 @@
-from flask import Flask
 from watson_developer_cloud import conversation_v1
 
-app = Flask(__name__)
+json = """
+{
+  "intents": [
+    {
+      "intent": "destination",
+      "confidence": 0.3521221876761777
+    }
+  ],
+  "entities": [
+    {
+      "entity": "sys-date",
+      "location": [
+        0,
+        7
+      ],
+      "value": "2016-12-09",
+      "metadata": {
+        "calendar_type": "GREGORIAN"
+      }
+    },
+    {
+      "entity": "sys-time",
+      "location": [
+        0,
+        7
+      ],
+      "value": "18:00:00",
+      "metadata": {
+        "calendar_type": "GREGORIAN"
+      }
+    }
+  ],
+  "input": {
+    "text": "tonight"
+  },
+  "output": {
+    "log_messages": [],
+    "text": [],
+    "nodes_visited": [
+      "node_4_1481308591294"
+    ]
+  },
+  "context": {
+    "conversation_id": "6f4f9f93-36a0-41f8-93d6-8bd72c98e7ab",
+    "system": {
+      "dialog_stack": [
+        "node_4_1481308591294"
+      ],
+      "dialog_turn_counter": 3,
+      "dialog_request_counter": 3
+    },
+    "departure": "BOS"
+  }
+}
+"""
 
-
-
-@app.route("/api/message")
-def getResponse(workspace_id, input_message):
+def getResponse(input_message):
     if input_message == None:
         input_message = ""
     return conversation_v1.message(workspace_id, input_message)
 
-
-if __name__ == "__main__":
-    app.run()
+def getResponseTest():
+    global json
+    return json
